6d77712cdeedb8ea3fffaf84a3244551e6ea08ee
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2024-03-22 22:54:55 +0100

    made the app killable only through activity monitor or shutdown

--- macos/youareanidiot-macos/AppDelegate.cs
+++ macos/youareanidiot-macos/AppDelegate.cs
@@ -5,18 +5,19 @@ namespace youareanidiot_macos;
 [Register("AppDelegate")]
 public class AppDelegate : NSApplicationDelegate
 {
-    public int windowCount = 0;
-    
+    public uint WindowCount = 0;
+
     public override void DidFinishLaunching(NSNotification notification)
     {
-        // Insert code here to initialize your application
     }
 
     public override void WillTerminate(NSNotification notification)
     {
-        // Oh buddy you have no idea what you've gotten yourself into
-        Process newProcess = new Process();
-        newProcess.StartInfo = new ProcessStartInfo(Environment.ProcessPath, (windowCount + 1).ToString());
-        newProcess.Start();
+    }
+    
+    public override NSApplicationTerminateReply ApplicationShouldTerminate(NSApplication sender)
+    {
+        // Prevent the app from closing
+        return NSApplicationTerminateReply.Cancel;
     }
 }
--- macos/youareanidiot-macos/ViewController.cs
+++ macos/youareanidiot-macos/ViewController.cs
@@ -4,13 +4,15 @@ namespace youareanidiot_macos;
 
 public partial class ViewController : NSViewController
 {
+    private AppDelegate GetDelegate() { return NSApplication.SharedApplication.Delegate as AppDelegate; }
     protected ViewController(NativeHandle handle) : base(handle)
     {
         // This constructor is required if the view controller is loaded from a xib or a storyboard.
         // Do not put any initialization here, use ViewDidLoad instead.
     }
 
-    public override void ViewWillDisappear()
+    public override void ViewDidLoad()
     {
+        
     }
 }
