c3a0479d700056fadf065df8399fa10f5ae8d8fb
AwMan <aw.man4814@gmail.com>
2026-07-26 15:46:59 +0200

    prevent user from moving the window out of screen bounds

--- youareanidiot/MainWindow.xaml.cs
+++ youareanidiot/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace youareanidiot;
 
@@ -8,10 +9,41 @@ namespace youareanidiot;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private readonly DispatcherTimer _locationChangeTimer;
+    private readonly Random _random = new Random();
+    
+    private readonly int _xBound, _yBound;
+
     public MainWindow()
     {
+        _locationChangeTimer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(250) };
+        _locationChangeTimer.Tick += OnLocationChangeEnded;
+
+        LocationChanged += OnLocationChanged;
         Closing += OnWindowClosing;
         InitializeComponent();
+        
+        _xBound = (int) (SystemParameters.WorkArea.Width - Width);
+        _yBound = (int) (SystemParameters.WorkArea.Height - Height);
+        
+        Left = _random.Next(1, _xBound);
+        Top = _random.Next(1, _yBound);
+    }
+
+    private void OnLocationChanged(object? sender, EventArgs e)
+    {
+        _locationChangeTimer.Stop();
+        _locationChangeTimer.Start();
+    }
+
+    private void OnLocationChangeEnded(object? sender, EventArgs e)
+    {
+        _locationChangeTimer.Stop();
+        
+        if (Left > _xBound)
+        { Left = _random.Next(1, _xBound); }
+        if (Top > _yBound)
+        { Top = _random.Next(1, _yBound); }
     }
 
     private static void OnWindowClosing(object? sender, CancelEventArgs e)
